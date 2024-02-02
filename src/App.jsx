bc882dd643c88326f0348e3483ff362c45abc409
eunji <bian87@dgu.ac.kr>
2024-02-03 01:53:20 +0900

    STYLE: 섹션 분리 main->app 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-import TestPage from "@/pages/test/TestPage.jsx";
+import TestPage from "/src/pages/test/TestPage.jsx";
+import { ContentWrapper, HeaderWrapper, LeftBody, MainWrapper, RightBody } from "/src/styles/style.js";
 
 // route 설정 해주세요.
 // 경로 "/"는 추후 각자 설정해주세요.
 function App() {
   return (
-    <>
+    <MainWrapper>
+    <LeftBody>
+
+    </LeftBody>
+    <RightBody>
+      <HeaderWrapper>
+      </HeaderWrapper>
+      <ContentWrapper>
       <Routes>
         <Route path="/test" element={<TestPage />} />
       </Routes>
-    </>
+      </ContentWrapper>
+    </RightBody>
+  </MainWrapper>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { theme } from "@/styles/theme.js";
-import GlobalStyle from "@/styles/GlobalStyle.js";
-import { ContentWrapper, HeaderWrapper, LeftBody, MainWrapper, RightBody } from "./styles/style.js";
+import { theme } from "/src/styles/theme.js";
+import GlobalStyle from "/src/styles/GlobalStyle.js";
 
 // mode는 다크모드, 화이트모드 등 전역 색상 용도, 색상은 theme 파일에서 import 해주세요.
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,18 +11,7 @@ root.render(
   <BrowserRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <MainWrapper>
-          <LeftBody>
-
-          </LeftBody>
-          <RightBody>
-            <HeaderWrapper>
-            </HeaderWrapper>
-            <ContentWrapper>
-              <App />
-            </ContentWrapper>
-          </RightBody>
-        </MainWrapper>
+       <App />
       </ThemeProvider>
   </BrowserRouter>
 );
