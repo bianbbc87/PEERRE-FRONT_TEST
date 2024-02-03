8ae2b4a3058119e37d82134dd62ca915270d54c4
eunji <bian87@dgu.ac.kr>
2024-02-04 02:49:32 +0900

    FEAT: gauge bar 구현

--- src/pages/team-report/TeamReport.jsx
+++ src/pages/team-report/TeamReport.jsx
@@ -1,7 +1,11 @@
 import { Container, Title, Team_Box, Gaze_Box, Gaze, Rank_Box } from "/src/styles/style";
-import GaugeChart from "react-gauge-chart";
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 export default function TeamReport() {
+  const progress = 80; // 진행 정도(%)
+
   return (
     <Container>
       <Title>
@@ -14,15 +18,21 @@ export default function TeamReport() {
         <p>팀원: 김서희. 진서희. 박서희, 최서희, 신서희, 유서희</p>
         <Gaze_Box>
           <p>동료평가 참여율</p>
-          <Gaze />
+          <Gaze>
+          </Gaze>
         </Gaze_Box>
       </Team_Box>
       <Rank_Box>
-      <GaugeChart id="gauge-chart2" 
-        nrOfLevels={30} 
-        percent={0.86} 
-        needleColor="#345243"
-      />
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ width: '100%', mr: 1 }}>
+            <LinearProgress variant="determinate" value={progress} />
+          </Box>
+          <Box sx={{ minWidth: 35 }}>
+            <Typography variant="body2" color="#000000">{`${Math.round(
+              progress,
+            )}%`}</Typography>
+          </Box>
+        </Box>
       </Rank_Box>
     </Container>
   )
