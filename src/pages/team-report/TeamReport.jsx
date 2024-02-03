23499cfa3aac2e272fa1ec6d73b91e541e24f899
eunji <bian87@dgu.ac.kr>
2024-02-04 02:29:42 +0900

    Merge branch 'main' of https://github.com/PEER-Re/PEERRE-FRONT into feature/team-report

--- src/pages/team-report/TeamReport.jsx
+++ src/pages/team-report/TeamReport.jsx
@@ -1,9 +1,24 @@
-import { Container } from "/src/styles/style";
+import { Container, Title, Team_Box, Gaze_Box, Gaze, Rank_Box } from "/src/styles/style";
 
 export default function TeamReport() {
   return (
     <Container>
+      <Title>
+        <p>
+          팀 리포트
+        </p>
+      </Title>
+      <Team_Box>
+        <p>팀 이름: PEER:Re</p>
+        <p>팀원: 김서희. 진서희. 박서희, 최서희, 신서희, 유서희</p>
+        <Gaze_Box>
+          <p>동료평가 참여율</p>
+          <Gaze />
+        </Gaze_Box>
+      </Team_Box>
+      <Rank_Box>
 
+      </Rank_Box>
     </Container>
   )
 }
--- src/styles/style.js
+++ src/styles/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import BG from "/src/assets/images/background/background.svg";
 
 // 페이지별로 용도 나눠주세요
 /**  샘플 페이지 스타일 **/
@@ -17,7 +18,7 @@ export const MainWrapper = styled.div`
 export const LeftBody = styled.div`
 top: 0;
 left: 0;
-width: 240px;;
+min-width: 240px;
 height: 100%;
 min-height: 100vh;
 background-color: #fff;
@@ -28,6 +29,9 @@ z-index: 100;
 
 export const RightBody = styled.div`
 flex-grow: 1; /* 남은 공간을 전부 차지한다. */
+display: flex;
+flex-direction: column;
+/* flex, flex-direction을 해주어야 height도 전체 차지함. */
 height: 100%;
 min-height: 100vh;
 margin: 0;
@@ -39,15 +43,15 @@ height: 10vh;
 background-color: #fff; 
 margin: 0;
 box-shadow: 0px 0px 12.9px 1px rgba(0, 0, 0, 0.2);
+z-index: 50;
 `;
 
 export const ContentWrapper = styled.div`
 flex-grow: 1;
-width: 100%;
-height: 100%;
-background-color: white;
 z-index: 100;
 margin: 0;
+background: url(${BG});
+background-repeat: no-repeat;
 `;
 
 /* team-report */
@@ -57,5 +61,53 @@ position: relative;
 display: flex;
 flex-direction: column;
 align-items: center;
-text-align: center;
+padding: 20px 80px;
+text-align: left;
+`;
+
+export const Title = styled.p`
+text-align: left;
+display: flex;
+width: 100%;
+
+p {
+  text-align: left;
+font-size: 52px;
+font-weight: 800;
+margin: 0;
+}
+`;
+
+export const Team_Box = styled.div`
+width: 100%;
+height: 30%;
+display: flex;
+flex-direction: column;
+border-radius: 18px;
+border: 4px solid rgba(26, 208, 121, 0.5);
+text-align: left;
+padding: 20px 30px;
+gap: 20px;
+background-color: #fff;
+
+p {
+  font-size: 24px;
+  font-weight: 800;
+  margin: 0;
+}
+`;
+
+export const Gaze_Box = styled.div`
+display: flex;
+`;
+
+export const Gaze = styled.div`
+width: 70%;
+height: 20%;
+`;
+
+export const Rank_Box = styled.div`
+width: 100%;
+height: 70%;
+background-color: green;
 `;
