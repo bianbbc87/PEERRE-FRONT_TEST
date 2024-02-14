f2f5ea59e630d6383fdec0c6a5dec50e19e0f9f3
chan_nie <126068623+chennielee@users.noreply.github.com>
2024-02-14 13:40:10 +0900

    fix teamspace api code

--- src/pages/create-team/CreateTeam.jsx
+++ src/pages/create-team/CreateTeam.jsx
@@ -25,30 +25,33 @@ import {
 function CreateTeam() {
   const navigate = useNavigate();
 
-  const handleNextBoxClick = () => {
-    const userAccessToken =
-      "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcwOTkxMTQzNCwic29jaWFsSWQiOiJ0aGRkbXMyMDA5QG5hdmVyLmNvbSJ9.Kd3e8Xm2k_SgnyWMf84p7WPd9FzNwBF7VDLSD7h55my8J—xBuYNjKM8mexLg5oPVSHr7sHchssKMRNKpVPx2A";
-
+  const handleNextBoxClick = async () => {
     const postData = {
       name: teamName,
       profile: teamIntro,
     };
 
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + userAccessToken,
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcwOTkxMTQzNCwic29jaWFsSWQiOiJ0aGRkbXMyMDA5QG5hdmVyLmNvbSJ9.Kd3e8Xm2k_SgnyWMf84p7WPd9FzNwBF7VDLSD7h55my8J—xBuYNjKM8mexLg5oPVSHr7sHchssKMRNKpVPx2A`,
+      },
     };
 
-    axios
-      .post("http://13.124.90.245:8080/api/teamspace", postData, headers)
-      .then((response) => {
-        const teamspaceId = response.data.teamspaceId;
-        console.log(teamspaceId);
-        navigate(`/create-project`, { state: { teamspaceId } });
-      })
-      .catch((error) => {
-        console.error("팀 생성 요청 중 오류가 발생했습니다:", error);
-      });
+    try {
+      // console.log(config);
+      // console.log(postData);
+      const response = await axios.post(
+        "http://13.124.90.245:8080/api/teamspace",
+        postData,
+        config
+      );
+      const teamspaceId = response.data.teamspaceId;
+      console.log(response);
+      navigate(`/create-project`, { state: { teamspaceId } });
+    } catch (error) {
+      console.error("팀 생성 요청 중 오류가 발생했습니다:", error);
+    }
   };
 
   const [teamName, setTeamName] = useState("");
